1e32820ab38d7da4a17029d1a655b0e2ada56b1c
Tien Thanh <34149997+thanhkaiba@users.noreply.github.com>
2024-08-16 07:52:28 +0700

    Update chacha20_test.go

--- chacha20_test.go
+++ chacha20_test.go
@@ -24,7 +24,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"gitlab.com/yawning/chacha20.git/internal/api"
+	"github.com/thanhkaiba/chacha20/internal/api"
 )
 
 // Test vectors taken from:
